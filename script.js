fecaa2da03bf1dbd9ad4727caea58ec2ba2a3c1e
Jstanion <123262263+Jstanion@users.noreply.github.com>
2023-03-11 14:10:17 -0600

    added event listener to save buttons

--- script.js
+++ script.js
@@ -2,15 +2,18 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(document).ready(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-    //use ":button" for local storage save
-    $(":button").on("click", function() {
-      //$(this) -->how to save to local storage in jquery//
+
+  // Add a listener for click events on the save button
+  $('.saveBtn').on('click', function() {
+
+    // Get the user input from the textarea element
+    var userInput = $(this).siblings('.description').val();
+
+    // Get the id of the time-block containing the button that was clicked
+    var timeBlockId = $(this).parent().attr('id');
+    
+    // Save the user input in local storage using the id as the key
+    localStorage.setItem(timeBlockId, userInput);
     });
 
 
